9de51cb1a7cca664eccacd5a6c08dff6ef5e90fd
TUIRAN QUINTERO NATALIA <ntuiran@cuc.edu.co>
2022-03-14 20:20:57 -0500

    promesas - asincronismo

--- index.js
+++ index.js
@@ -120,3 +120,44 @@ const web = {
 
 const { enlace, name } = web.redeSociales.youtube;
 console.log(enlace);
+
+//PROMESAS
+
+fetch(' https://pokeapi.co/api/v2/pokemon')
+  .then(res => res.json())
+  .then(data => {
+    data.results.forEach(element => {
+      console.log(element.name)
+    });
+  })
+  .catch(error => console.log(error))
+
+//MEJORAS DE LAS PROMESAS - ASINCRONISMO
+
+const getPokemon = async () => {
+  try {
+    const res = await fetch("https://pokeapi.co/api/v2/pokemon");
+    const data = await res.json();
+    console.log(data.results);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+getPokemon();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
